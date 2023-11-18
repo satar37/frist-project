ada5bb56c3186bacada8ebb6c8605425a22c496a
satar37 <151363397+satar37@users.noreply.github.com>
2023-11-18 12:51:23 +0300

    Add files via upload

---
+++ apply/apply/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace apply
+{
+    internal class Program
+    {
+
+        abstract class  Customer
+        {
+            int Id { get; set; }
+            string Name { get; set; }
+            int   Age { get; set; }
+            double Salary { get; set; }
+            int State { get; set; }
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
